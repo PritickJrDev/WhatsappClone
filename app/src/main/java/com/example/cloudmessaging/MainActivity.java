5ff207a06aba3b7f407dd08db66f5584904e6423
Prithiraj Mahilary <98103024+PritickJrDev@users.noreply.github.com>
2023-05-07 05:14:02 +0530

    first commit

--- app/src/main/java/com/example/cloudmessaging/MainActivity.java
+++ app/src/main/java/com/example/cloudmessaging/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.cloudmessaging;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView titleTV;
+    private EditText usernameET, passwordET, emailET;
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
