c54bc8a6d0b0eee49a9760499448af035bfc1252
Prithiraj Mahilary <98103024+PritickJrDev@users.noreply.github.com>
2023-05-15 23:35:41 +0530

    Update ChatsFragment.java

--- app/src/main/java/com/example/cloudmessaging/fragments/ChatsFragment.java
+++ app/src/main/java/com/example/cloudmessaging/fragments/ChatsFragment.java
@@ -45,7 +45,7 @@ public class ChatsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        
         View view = inflater.inflate(R.layout.fragment_chats, container, false);
 
         recyclerView = view.findViewById(R.id.recycler_view);
